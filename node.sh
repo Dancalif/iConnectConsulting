e744a2170a4dee7edea3f92231622a21081ed034
Dancalif <dancalif@gmail.com>
2016-08-24 22:27:06 -0700

    Run a node from the work dir

--- node.sh
+++ node.sh
@@ -1,1 +1,1 @@
-/usr/bin/java -jar /Users/dancalif/Downloads/selenium-server-standalone-2.53.1.jar -role node  -hub http://localhost:4444/grid/register > selenium.out &
+/usr/bin/java -jar selenium-server-standalone-2.53.1.jar -role node  -hub http://localhost:4444/grid/register > selenium.out &
