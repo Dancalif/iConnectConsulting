73f445191e6cd0be5aa5a184722a8b044932fb7d
labwebportal <denisumanetssf@gmail.com>
2017-03-21 19:45:53 -0700

    test

--- src/test/java/iConnectConsulting/MainTest.java
+++ src/test/java/iConnectConsulting/MainTest.java
@@ -1,13 +1,10 @@
 package iConnectConsulting;
 
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -16,34 +13,31 @@ import org.testng.annotations.Parameters;
 
 import com.iConnectConsulting.data.UserData;
 import com.iConnectConsulting.pageobjects.CommonElements;
-import com.iConnectConsulting.pageobjects.DashBoardPage_PO;
 import com.iConnectConsulting.pageobjects.SignInPage_PO;
-
-import net.sf.cglib.core.Local;
 
 public class MainTest {
 
 	WebDriver driver;
-	UserData user = new UserData("ptox", "ptox2013");
+	UserData user = new UserData("yourUsername", "yourPassword");
 
 	@BeforeMethod(alwaysRun = true)
 	@Parameters({ "browser", "url" })
 	public WebDriver setUp(String browser, String url) throws Exception {
-			// Launch the FireFox browser
-			if (browser.equalsIgnoreCase("firefox")) {
-				driver = new FirefoxDriver();
-			} else if (browser.equalsIgnoreCase("chrome")) {
-				System.setProperty("webdriver.chrome.driver",
-						"/Users/dan/Dropbox/iConnectConsulting_workspace/iconnectconsulting_dev/webDrivers/chromedriver");
-				driver = new ChromeDriver();
-			}
-			// Navigate to AUT, managing the browser window
-			driver.manage().deleteAllCookies();
-			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			driver.get(url);
-			return driver;
+		// Launch the FireFox browser
+		if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"/Users/dan/Dropbox/Portfolio_workspace/iconnectconsulting_dev/webDrivers/chromedriver");
+			driver = new ChromeDriver();
 		}
+		// Navigate to AUT, managing the browser window
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(url);
+		return driver;
+	}
 
 	@AfterMethod(alwaysRun = true)
 	public void tearDown() throws Exception {
